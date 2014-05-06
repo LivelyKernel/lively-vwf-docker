b5e6ddb92486b3fd77084eb6672139bd4c64e7de
Robert Krahn <robert.krahn@gmail.com>
2014-05-06 23:34:50 +0000

    switching to current vwf branch

--- lively-vwf-docker/start-server.sh
+++ lively-vwf-docker/start-server.sh
@@ -6,6 +6,9 @@ VWF_PORT=${2-3000}
 docker run \
     -p $LV_PORT:$LV_PORT \
     -p $VWF_PORT:$VWF_PORT \
+    -p 3008:3008 \
+    -p 8080:8080 \
+    -p 8081:8081 \
     -e "livelyport=$LV_PORT" \
     -e "vwfport=$VWF_PORT" \
     -t -i old-lively-vwf-server
