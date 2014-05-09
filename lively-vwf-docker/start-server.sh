eed3291ddae4e4170da4d8eec14f9a8d5e611d3b
Robert Krahn <robert.krahn@gmail.com>
2014-05-09 23:06:44 +0000

    small update

--- lively-vwf-docker/start-server.sh
+++ lively-vwf-docker/start-server.sh
@@ -9,6 +9,7 @@ docker run \
     -p 3008:3008 \
     -p 8080:8080 \
     -p 8081:8081 \
+    -p 8082:8082 \
     -e "livelyport=$LV_PORT" \
     -e "vwfport=$VWF_PORT" \
-    -t -i old-lively-vwf-server
+    -t -i lively-vwf-server
