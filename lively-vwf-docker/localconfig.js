73dd1912740627617c6553500061a9b8864802aa
root <root@li639-59.members.linode.com>
2014-06-09 23:31:25 +0000

    lively-vwf cleanup

--- lively-vwf-docker/localconfig.js
+++ lively-vwf-docker/localconfig.js
@@ -1,14 +1,106 @@
 var c = lively.Config;
-c.set('lively2livelyAutoStart', false);
-c.set('lively2livelyEnableConnectionIndicator', false);
-c.set('removeDOMContentBeforeWorldLoad', false);
-c.set('manuallyCreateWorld', true);
+c.set('lively2livelyAutoStart', true);
+c.set('lively2livelyEnableConnectionIndicator', true);
+c.set('removeDOMContentBeforeWorldLoad', true);
+c.set('manuallyCreateWorld', false);
 c.set('changesetsExperiment', false);
+c.set('computeCodeEditorCompletionsOnStartup', false);
+c.addOption("loadVWF", true);
 
-// // FIXME, for compatibility with requirejs
-// lively.whenLoaded(function() {
-//     setTimeout(function() {
-// 	// hmm with fixes from patch not needed anymore...
-//         Global.require = Global.requirejs.s.contexts._.require;
-//     }, 300);
-// });
+lively.whenLoaded(function() {
+
+    (function() { lively.Config.get('loadVWF') && loadVWF(); }).delay(3);
+
+    // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+    function loadVWF() {
+        var vwfBaseURL = URL.root;
+
+        // Global.require = lively.require
+
+        Global.require = undefined;
+
+        var pBar = $world.addProgressBar(null, "loading VWF");
+
+        var loadSteps = [
+            // {url: "compatibilitycheck.js"},
+            {url: "socket.io.js"},
+            {url: "socket.io-sessionid-patch.js"},
+            {url: "require.js"},
+            {run: function() {
+                // Global.require = requirejs.s.contexts._.require;
+                Global.require = requirejs
+                requirejs.config({
+                    baseUrl: String(vwfBaseURL)
+                });
+            }},
+            {url: "async.js"},
+
+            // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+            // google libs
+            {url: "closure/base.js"},
+            // <script type="text/javascript" src="closure/base.js">
+            // </script><script type="text/javascript" src="http://23.92.25.59:3006/KIE7cX6ArICLKVLv/closure/deps.js"></script>
+
+            // <script type="text/javascript">goog.require('goog.vec.Mat4')</script>
+            {url: "closure/vec/float32array.js"},
+            {url: "closure/vec/float64array.js"},
+            {url: "closure/vec/vec.js"},
+            {url: "closure/vec/vec3.js"},
+            {url: "closure/vec/vec4.js"},
+            {url: "closure/vec/mat4.js"},
+
+            // <script type="text/javascript">goog.require('goog.vec.Quaternion')</script>
+            {url: "closure/vec/quaternion.js"},
+
+            {url: "crypto.js"},
+            {url: "md5.js"},
+            {url: "alea.js"},
+            {url: "mash.js"},
+            {url: "Class.create.js"},
+            // {url: "rAF.js"},
+            // {url: "performance.now-polyfill.js"},
+            {url: "vwf.js"},
+            {run: function(next) {
+                define("jquery", function() { return lively.$; });
+                require(["jquery"], next)
+            }}, {
+                run: function(next) {
+                    var userLibraries = {model:{}, view:{}};
+                    var application = window.application = undefined;
+                    vwf.loadConfiguration(application, userLibraries, next);
+
+            }}
+        ];
+
+        loadSteps.doAndContinue(function(next, step, i) {
+
+            if (pBar) pBar.setValue(loadSteps.length / i);
+
+            if (step.url) {
+                var url = String(vwfBaseURL.withFilename(step.url));
+                JSLoader.removeAllScriptsThatLinkTo(url);
+                JSLoader.loadedURLs.remove(url);
+                JSLoader.loadJs(url, function() {
+                    alertOK('Done loading ' + url);
+                    next();
+                });
+            } else if (step.run) {
+                var takesCallback = step.run.length > 0;
+                try {
+                    step.run(next);
+                    alertOK('Done running %s', String(step.run).truncate(70));
+                } catch (e) {
+                    show('step.run error:\n%s', e.stack || e);
+                }
+                !takesCallback && next();
+            } else {
+                show('Cannot deal with step:\n%o', step);
+                next();
+            }
+
+        }, function() { pBar && pBar.remove(); });
+
+    }
+
+});
--- lively-vwf-docker/start-server.sh
+++ lively-vwf-docker/start-server.sh
@@ -6,10 +6,19 @@ VWF_PORT=${2-3000}
 docker run \
     -p $LV_PORT:$LV_PORT \
     -p $VWF_PORT:$VWF_PORT \
-    -p 3008:3008 \
-    -p 8080:8080 \
-    -p 8081:8081 \
-    -p 8082:8082 \
+    -P \
     -e "livelyport=$LV_PORT" \
     -e "vwfport=$VWF_PORT" \
+    -e "vwfapp=/var/www/vwf/public" \
     -t -i lively-vwf-server
+
+# docker run \
+#     -p $LV_PORT:$LV_PORT \
+#     -p $VWF_PORT:$VWF_PORT \
+#     -p 3008:3008 \
+#     -p 8080:8080 \
+#     -p 8081:8081 \
+#     -p 8082:8082 \
+#     -e "livelyport=$LV_PORT" \
+#     -e "vwfport=$VWF_PORT" \
+#     -t -i lively-vwf-server
